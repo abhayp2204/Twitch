d1bf8613dedd8519f3700d598de533bbbf2cafee
abhayp2204 <abhaypatil1020@gmail.com>
2023-04-09 11:01:15 +0530

    video player

--- twitch2/src/components/Room.js
+++ twitch2/src/components/Room.js
@@ -1,14 +1,19 @@
 import React, { useState, useContext } from 'react'
 import Chat from './Chat'
-
 import { UserContext } from '../App'
+import '../css/Room.css'
 
 function Room(props) {
-    return (
-        <div className='room'>
-            <Chat user={props.user} />
-        </div>
-    )
+  const [videoUrl, setVideoUrl] = useState('https://www.youtube.com/embed/dQw4w9WgXcQ')
+
+  return (
+    <div className='room'>
+      <Chat user={props.user} />
+      <div className='video-container'>
+        <iframe src={videoUrl} frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+      </div>
+    </div>
+  )
 }
 
 export default Room
