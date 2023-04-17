ea4acbb037991aa68ea046b9bd827ee63774f910
abhayp2204 <abhaypatil1020@gmail.com>
2023-04-18 04:10:35 +0530

    desc

--- twitch4/client/twitch4/src/data/rooms.js
+++ twitch4/client/twitch4/src/data/rooms.js
@@ -2,7 +2,7 @@ const rooms = [
     {
         value: "skyrim",
         label: "Skyrim",
-        desc: "The Elder Scrolls V: Skyrim is an action role-playing video game developed by Bethesda Game Studios and published by Bethesda Softworks. ",
+        desc: "In the frozen wastes to the north of Skyrim, sinister forces are mustering. Dragons, long lost to the passages of the Elder Scrolls, have returned to Tamriel. The future of Skyrim, even the Empire itself, hangs in the balance as they wait for the prophesied Dragonborn to come; a hero born with the power of The Voice, and the only one who can stand amongst the dragons.",
         origurl: 'https://www.youtube.com/watch?v=OkFdqqyI8y4',
         url: "https://www.youtube.com/embed/OkFdqqyI8y4",
         videoId: "OkFdqqyI8y4",
@@ -10,7 +10,7 @@ const rooms = [
     {
         value: "valorant",
         label: "Valorant",
-        desc: "Valorant is a free-to-play multiplayer first-person shooter developed and published by Riot Games. ",
+        desc: "In Valorant, powerful agents are locked in tactical showdowns. Play as a unique character with abilities and weapons you won’t find anywhere else. Every match is an intense 5v5 battle between two teams, where you’ll need to make split-second decisions and coordinate with your team. Take on the role of a Sentinel, Controller, Duelist or Initiator and use your abilities to create opportunities, find openings, and gain an edge over the competition.",
         origurl: 'https://www.youtube.com/watch?v=6fm-uj8E8lI',
         url: "https://www.youtube.com/embed/6fm-uj8E8lI",
         videoId: "6fm-uj8E8lI",
@@ -26,7 +26,7 @@ const rooms = [
     {
         value: "geoguesser",
         label: "GeoGuesser",
-        desc: "GeoGuessr is a geography game which takes you on a journey around the world and challenges your ability to recognize your surroundings.",
+        desc: "GeoGuessr is a geography game which takes you on a journey around the world and challenges your ability to recognize your surroundings. You will be dropped somewhere in the world and your goal is to find clues and make educated guesses to determine your exact location on the map. The game is played in rounds, and each round you will be dropped somewhere new. The more accurate your guess, the more points you will earn.",
         origurl: 'https://www.youtube.com/watch?v=2KXcQ18JyMs',
         url: "https://www.youtube.com/embed/2KXcQ18JyMs",
         videoId: "2KXcQ18JyMs",
@@ -34,7 +34,7 @@ const rooms = [
     {
         value: "music",
         label: "Music",
-        desc: "Waka Waka (This Time for Africa) is the official song of the 2010 FIFA World Cup, performed by Colombian singer Shakira featuring South African band Freshlyground.",
+        desc: "Waka Waka is a song by Colombian singer Shakira, featuring South African band Freshlyground. It was released as the official song of the 2010 FIFA World Cup on 4 May 2010. The song was written by Shakira and Freshlyground's lead singer, Zolani Mahola, and produced by Shakira and John Hill. The song was recorded in English, Spanish, French, Portuguese, and Arabic.",
         origurl: 'https://www.youtube.com/watch?v=pRpeEdMmmQ0',
         url: "https://www.youtube.com/embed/pRpeEdMmmQ0",
         videoId: "pRpeEdMmmQ0",
@@ -51,7 +51,7 @@ const rooms = [
     {
         value: "runescape",
         label: "Runescape",
-        desc: "Runescape is a free-to-play massively multiplayer online role-playing game (MMORPG) developed and published by Jagex, first released in January 2001.",
+        desc: "Runescape is a fantasy MMORPG developed and published by Jagex. It was originally released in January 2001 and is the world's most popular free-to-play MMORPG, with over 200 million registered accounts, as of February 2019. The game is set in the fictional kingdom of Gielinor, which is divided into different kingdoms, regions, and cities. Players control a character, known as a player character (PC), and explore the game world completing quests and defeating monsters to gain experience and items.",
         origurl: 'https://www.youtube.com/watch?v=l0f6c2fy35E',
         url: "https://www.youtube.com/embed/l0f6c2fy35E",
         videoId: "l0f6c2fy35E",
